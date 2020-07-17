cca6c5defd2b5eb9d15f39d316baed4de5540f3d
leogtzr <leogutierrezramirez@gmail.com>
2020-07-16 19:48:08 -0600

    add code snippets for hot streams

---
+++ reactive-snippets/src/test/java/com/playground/HotStreamTest1.java
@@ -0,0 +1,41 @@
+package com.playground;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.EmitterProcessor;
+import reactor.core.publisher.FluxSink;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class HotStreamTest1 {
+
+    @Test
+    public void hot() throws Exception {
+        var first = new ArrayList<Integer>();
+        var second = new ArrayList<Integer>();
+
+        final EmitterProcessor<Integer> emitter = EmitterProcessor.create(2);
+        final FluxSink<Integer> sink = emitter.sink();
+
+        // emitter.subscribe(first::add);
+        emitter.subscribe(collect(first));
+        sink.next(1);
+        sink.next(2);
+
+        // emitter.subscribe(second::add);
+        emitter.subscribe(collect(second));
+        sink.next(3);
+        sink.complete();
+
+        Assertions.assertTrue(first.size() > second.size());
+
+    }
+
+    private Consumer<Integer> collect(final List<Integer> collection) {
+        return collection::add;
+    }
+
+}
