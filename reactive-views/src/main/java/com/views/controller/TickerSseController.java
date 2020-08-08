69a9dd84b3ba13701ea11e5448d54403f51d4624
leogtzr <leogutierrezramirez@gmail.com>
2020-08-07 23:06:10 -0600

    remove unnecessary import

--- reactive-views/src/main/java/com/views/controller/TickerSseController.java
+++ reactive-views/src/main/java/com/views/controller/TickerSseController.java
@@ -7,8 +7,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.thymeleaf.spring5.context.webflux.ReactiveDataDriverContextVariable;
 
-import java.awt.*;
-
 @Controller
 public class TickerSseController {
 
